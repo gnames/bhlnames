8f5cec1957a102e43ae58d8e9905657726f2c04d
Dmitry Mozzherin <dmozzherin@gmail.com>
2022-01-19 11:51:45 -0600

    small fixes

--- io/bayesio/data/stats.rb
+++ io/bayesio/data/stats.rb
@@ -1,19 +1,18 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'json'
 
-txt = File.read("bayes.json")
+txt = File.read('bayes.json')
 data = JSON.parse(txt, symbolize_names: true)
 nomen = data[:labelCases][:isNomen].to_f
 not_nomen = data[:labelCases][:notNomen].to_f
 
-features = [:annot, :title, :vol, :yrPage, :bestRes, :resNum]
+features = %i[annot title vol yrPage bestRes resNum]
 
 features.each do |f|
   data[:featureCases][f].each do |k, v|
-    likelihood = (v[:isNomen].to_f/nomen)/(v[:notNomen].to_f/not_nomen)
+    likelihood = (v[:isNomen].to_f / nomen) / (v[:notNomen].to_f / not_nomen)
     puts format("%s:%s,%0.3f", f, k, likelihood)
   end
 end
-
-
