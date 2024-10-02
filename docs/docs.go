a01187d9f3bff5b90d5b2986f8d748fb6c09079d
Dmitry Mozzherin <dmozzherin@gmail.com>
2024-10-02 16:58:16 +0000

    (fix #64)

--- docs/docs.go
+++ docs/docs.go
@@ -1028,7 +1028,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "bhlnames.globalnames.org",
+	Host:             "localhost:8888",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "BHLnames API",
